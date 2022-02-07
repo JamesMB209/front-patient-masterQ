60a9b910bcbff713137ba5c8de276bdb5a4c3e38
JamesMB209 <jmb209@gmail.com>
2022-02-07 21:38:30 +0800

    finsihed review page for pris

--- src/redux/patientObj/actions.js
+++ src/redux/patientObj/actions.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 export const LOAD_OBJ = "LOAD_OBJ";
+export const SUBMIT_REVIEW = "SUBMIT_REVEIW";
+const token = localStorage.getItem("token");
 
 export function loadObjThunk(data) {
-    console.log(data)
     return (dispatch) => {
-        let token = localStorage.getItem("token");
         return axios({
             method: 'post',
             url: `${process.env.REACT_APP_API_SERVER}/obj/patient`,
@@ -23,6 +23,35 @@ export function loadObjThunk(data) {
                     payload: response.data
                 })
 
+            })
+            .catch((err) => {
+                console.log('Error actions patient object')
+                console.error(err)
+            })
+    }
+}
+
+
+// not sure if I should move this into another catagory, but I want to effect the patientOBJ's status so ill leave it here because it contains the most relevant reducer.
+export function submitReviewThunk (data) {
+    console.log("trying to submit a review")
+    console.log(data)
+    return (dispatch) => {
+        return axios({
+            method: 'post',
+            url: `${process.env.REACT_APP_API_SERVER}/review/submit`,
+            headers: { Authorization: `Bearer ${token}` },
+            data: {
+                appointmentHistoryID: data.appointmentHistoryID,
+                score: data.score,
+                review: data.review,
+            }
+        })
+            .then((response) => {
+                console.log(response)
+                return dispatch({
+                    type: SUBMIT_REVIEW,
+                })
             })
             .catch((err) => {
                 console.log('Error actions patient object')
